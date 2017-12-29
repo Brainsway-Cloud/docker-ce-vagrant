e4b7b43c5e9d51839cbab9a6eeb91bd2e78706a5
Brainsway-Cloud <34479918+Brainsway-Cloud@users.noreply.github.com>
2017-12-29 14:24:24 +0200

    initial version

---
+++ install-couch.sh
@@ -0,0 +1,42 @@
+        #!/bin/bash -v
+        
+        # Download and run scripts to configure Ubuntu and Docker
+        git clone https://github.com/redgeoff/docker-ce-vagrant
+        cd docker-ce-vagrant
+        ./ubuntu.sh # Select "keep the local version ... "
+        ./docker.sh
+        
+        # Create a directory for hosting your DB files
+        mkdir /home/ubuntu/common
+        
+        # Run a CouchDB Docker Container. 
+        # (Use EC2 metadata URL to get machine local IP.)
+        # (password=admin. use couchdb-hash-pwd -p my-password to generate new COUCHDB_HASHED_PASSWORD)
+        docker run -d --name couchdb \
+          --restart always \
+          -p 5984:5984 -p 5986:5986 -p 4369:4369 -p 9100-9200:9100-9200 \
+          -v /home/ubuntu/common:/home/couchdb/common \
+          -e COUCHDB_DATA_DIR="/home/couchdb/common/data" \
+          -e COUCHDB_USER='admin' \
+          -e COUCHDB_HASHED_PASSWORD='-pbkdf2-b1eb7a68b0778a529c68d30749954e9e430417fb,4da0f8f1d98ce649a9c5a3845241ae24,10' \
+          -e COUCHDB_COOKIE='mycookie' \
+          -e COUCHDB_SECRET='mysecret' \
+          -e COUCHDB_NODE_NAME=`(curl http://169.254.169.254/latest/meta-data/local-ipv4)` \
+          redgeoff/couchdb
+          
+        # Enable CORS so that your application can communicate with the database from another domain/subdomain.
+        curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+        apt-get install -y nodejs build-essential
+        npm install npm -g
+        npm install -g add-cors-to-couchdb
+        add-cors-to-couchdb http://localhost:5984 -u admin -p admin
+
+        # If peer specified, create the 2-node cluster
+        if [ -z ${1+x} ]
+        then 
+            echo ""
+        else
+            git clone https://github.com/redgeoff/redgeoff-couchdb-docker
+            cd redgeoff-couchdb-docker
+            ./create-cluster.sh admin admin 5984 5986 \"$1 `(curl http://169.254.169.254/latest/meta-data/local-ipv4)`\"
+        fi
