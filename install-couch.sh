cb1129f72c7fa5d5909fd2481a0582b7fa90e233
Brainsway-Cloud <34479918+Brainsway-Cloud@users.noreply.github.com>
2018-03-14 22:47:34 +0200

    Update install-couch.sh

--- install-couch.sh
+++ install-couch.sh
@@ -4,6 +4,15 @@
         ./ubuntu.sh # Select "keep the local version ... "
         ./docker.sh
         
+        LOCAL_IP=`(curl http://169.254.169.254/latest/meta-data/local-ipv4)`
+        USERNAME=$1
+        PASSWORD=$2
+        HASH=$3
+        COOKIE=$4
+        SECRET=$5      
+        
+        '-pbkdf2-b1eb7a68b0778a529c68d30749954e9e430417fb,4da0f8f1d98ce649a9c5a3845241ae24,10'
+        
         # Create a directory for hosting your DB files
         mkdir /home/ubuntu/common
         
@@ -15,18 +24,16 @@
           -p 5984:5984 -p 5986:5986 -p 4369:4369 -p 9100-9200:9100-9200 \
           -v /home/ubuntu/common:/home/couchdb/common \
           -e COUCHDB_DATA_DIR="/home/couchdb/common/data" \
-          -e COUCHDB_USER='admin' \
-          -e COUCHDB_HASHED_PASSWORD='-pbkdf2-b1eb7a68b0778a529c68d30749954e9e430417fb,4da0f8f1d98ce649a9c5a3845241ae24,10' \
-          -e COUCHDB_COOKIE='mycookie' \
-          -e COUCHDB_SECRET='mysecret' \
+          -e COUCHDB_USER=$USERNAME \
+          -e COUCHDB_HASHED_PASSWORD=$HASH\
+          -e COUCHDB_COOKIE=$COOKIE \
+          -e COUCHDB_SECRET=$SECRET \
           -e COUCHDB_NODE_NAME=`(curl http://169.254.169.254/latest/meta-data/local-ipv4)` \
-          redgeoff/couchdb
-          
-        LOCAL_IP=`(curl http://169.254.169.254/latest/meta-data/local-ipv4)`
+          redgeoff/couchdb         
           
         docker run -it --name spiegel-install \
           -e TYPE='install' \
-          -e URL=http://admin:admin@$LOCAL_IP:5984 \
+          -e URL=http://$USERNAME:$PASSWORD@$LOCAL_IP:5984 \
           redgeoff/spiegel
           
         docker service create \
@@ -34,14 +41,14 @@
            --detach=true \
            --replicas 2 \
            -e TYPE='update-listener' \
-           -e URL='http://admin:admin@$LOCAL_IP:5984' \
+           -e URL='http://$USERNAME:$PASSWORD@$LOCAL_IP:5984' \
            redgeoff/spiegel
            
         docker service create \
            --name spiegel-replicator \
            --detach=true \
            --replicas 2 \
-           -e URL='http://admin:admin@$LOCAL_IP:5984' \
+           -e URL='http://$USERNAME:$PASSWORD@$LOCAL_IP:5984' \
            --mount type=bind,source=replicator-passwords.json,destination=/usr/src/app/passwords.json \
            -e PASSWORDS_FILE=/usr/src/app/passwords.json \
            redgeoff/spiegel
@@ -51,15 +58,15 @@
         apt-get install -y nodejs build-essential
         npm install npm -g
         npm install -g add-cors-to-couchdb
-        add-cors-to-couchdb http://localhost:5984 -u admin -p admin
+        add-cors-to-couchdb http://localhost:5984 -u $USERNAME -p $PASSWORD
 
         # If peer specified, create the 2-node cluster
-        if [ -z ${1+x} ]
+        if [ -z ${6+x} ]
         then 
             echo ""
         else
             git clone https://github.com/redgeoff/redgeoff-couchdb-docker
             cd redgeoff-couchdb-docker
             # See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html for how meta-data works
-            ./create-cluster.sh admin admin 5984 5986 $1 $LOCAL_IP
+            ./create-cluster.sh $USERNAME $PASSWORD 5984 5986 $6 $LOCAL_IP
         fi
